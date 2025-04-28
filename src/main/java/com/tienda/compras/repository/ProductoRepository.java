46cb856776138428c24c7cd5ad90e776e5b743ad
belisario-aldair <aldair.belisario@estudiante.ucsm.edu.pe>
2025-04-28 15:25:53 -0500

    Aplicación Base
    Aplicacion Base

---
+++ src/main/java/com/tienda/compras/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.tienda.compras.repository;
+
+import com.tienda.compras.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
