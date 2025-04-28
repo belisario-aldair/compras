acda91e26d95cbc54b02dcc5078c2fdd355aaa01
belisario-aldair <aldair.belisario@estudiante.ucsm.edu.pe>
2025-04-28 15:27:28 -0500

    Delete src/main directory

--- src/main/java/com/tienda/compras/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tienda.compras.repository;
-
-import com.tienda.compras.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
